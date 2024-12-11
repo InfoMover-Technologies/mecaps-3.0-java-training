aad82e61ff5081be0629333c8d033731703d2ce8
Muhammed Shakir <shakir@infomover.io>
2024-12-11 11:34:59 +0530

    Getting started with Streams

--- src/main/java/com/im/trainings/java/MoreOnStreams.java
+++ src/main/java/com/im/trainings/java/MoreOnStreams.java
@@ -1,19 +1,97 @@
 package com.im.trainings.java;
 
-import com.im.trainings.java.model.Customer;
+import com.im.trainings.java.model.Employee;
 import com.im.trainings.java.model.ExportCustomer;
 import com.im.trainings.java.model.Invoice;
 import com.im.trainings.java.model.InvoiceVO;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+
+/**
+ * We have covered the following topics in streams
+ * <hr/>
+ * <ol>
+ *     <li>.stream() on any collection will provide a stream of elements in that collection,
+ *     so that you can iterate over its elements</li>
+ *
+ *     <li>List, Set, Map (key, value) | ConcurrentHashMap data structures</li>
+ *     <li>Functional interfaces : java.util.function.Function, BiFunction, Predicate, ByPredicate, Consumer, BiConsumer</li>
+ *
+ *     <li>You stream over elements to ?
+ *     <ol>
+ *         <li>.filter() : using Predicate</li>
+ *         <li>.map() : to perform transformations - using Function</li>
+ *         <li>.findFirst() : to perform transformations - using Function</li>
+ *         <li>.anyMatch() : to perform transformations - using Function</li>
+ *         <li>.findAny() : to perform transformations - using Function</li>
+ *     </ol>
+ *     </li>
+ *     <li>
+ *         Optional<Integer> : empty() true if no Integer in it or else it will return false
+ *     </li>
+ *
+ * </ol>
+ * <p>
+ * <hr/>
+ * things to discuss
+ * <ol>
+ *     <li>method reference</li>
+ *     <li>syntactic sugar</li>
+ * </ol>
+ */
 public class MoreOnStreams {
 
     public static void main(String[] args) {
+
+        Map<String, Employee> employeeMap = new HashMap<>();
+        employeeMap.put("101", new Employee(101, "Pranjal", "Bhopal"));
+        employeeMap.put("102", new Employee(102, "Rehan", "Bhopal"));
+        employeeMap.put("103", new Employee(103, "Umar", "Bhopal"));
+
+
+        Employee e = employeeMap.get("101");
+        System.out.println(e);
+
+//        Map<String, Employee> demo = new HashMap<>();
+
+//        Set<Map.Entry<String, Employee>> entrySet =
+        List<String> empNames = employeeMap.entrySet()
+                .stream()
+                .peek(x -> {
+
+                    String key = x.getKey();
+                    Employee emp = x.getValue();
+                    //                    System.out.println(key + " -- " + emp);
+
+                })
+//                .map(x -> {
+//                    String key = x.getKey();
+//                    Employee emp = x.getValue();
+//                    /**
+//                     *
+//                     */
+//                    return emp.getName();
+//
+//                })
+                // static method reference (referring to the method that is static in the class)
+                .map(MoreOnStreams::transformEntryOfStringAndEmployeeToNameOfEmployee)
+                .collect(Collectors.toList());
+
+        System.out.println(empNames);
+//        empNames.forEach(x -> {
+//            System.out.println(x);
+//        });
+        // referring to the object method: println is the method in the object called out which is declared as static in System
+        empNames.forEach(System.out::println);
+
+        if (true) {
+            return;
+        }
 
         // Assume that the invoices are obtained by the database call
         // And also assume that there are 1000 invoices.
@@ -32,7 +110,7 @@ public class MoreOnStreams {
 
         Map<Integer, ExportCustomer> customerMap = allCustomers.stream()
                 .collect(Collectors.toMap(
-                        ExportCustomer::getId,     // key mapper - gets the ID
+                        ExportCustomer::getId,
                         Function.identity()        // value mapper - uses the entire customer object
                 ));
 
@@ -62,6 +140,17 @@ public class MoreOnStreams {
                 })
                 .collect(Collectors.toList());
 
+
+    }
+
+    public static String transformEntryOfStringAndEmployeeToNameOfEmployee(Map.Entry<String, Employee> employeeEntry) {
+
+        String key = employeeEntry.getKey();
+        Employee emp = employeeEntry.getValue();
+        /**
+         *
+         */
+        return emp.getName();
 
     }
 }
